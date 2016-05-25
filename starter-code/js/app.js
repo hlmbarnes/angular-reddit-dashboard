2d6926e125a853c64b9441a60a81a372e818ea40
Hannah Barnes <hlmbarnes@me.com>
2016-05-25 07:28:25 -0700

    updated the angular ng repeats, search functionality and local storage

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,53 @@
-/* put js code here */
+var redditApp = angular.module('redditApp',[]);
+
+redditApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.searchTerm = '';
+  $scope.lastSearch = '';
+  $scope.lastIndex = '';
+  $scope.results = [];
+
+// LOCAL STORAGE
+  // var taco = [1,2,3];
+  // window.localStorage.taco=JSON.stringify(taco);
+  //converts the data to a string "[1,2,3]" and stores it in .taco
+  if(localStorage.length === 0) {
+     $scope.history = [];
+   } else {
+    $scope.history = JSON.parse(localStorage.getItem('$scope.history'));
+   }
+
+  $scope.remove = function(index){
+  $scope.history.splice(index, 1);
+  localStorage.setItem('$scope.history', JSON.stringify($scope.history));
+  }
+
+
+  $scope.search = function() {
+    var req = {
+      url: "http://www.reddit.com/search.json?q=" + $scope.searchTerm,
+      method: 'GET'
+    }
+    $http(req).then(function success(res) {
+    $scope.results = [];
+    var redditData = res.data.data.children
+    $scope.history.unshift($scope.searchTerm);
+       localStorage.setItem('$scope.history', JSON.stringify($scope.history));
+ 
+       for (i=0; i<redditData.length; i++){
+         var entry1 = redditData[i].data;
+         // console.log(entry1);
+         $scope.results.push(entry1);
+       }
+ 
+       // console.log($scope.results);
+     }, function error(res){
+       console.log("error: ",res)
+     })
+   }
+
+  
+
+
+}]);
 
 console.log('this file is running.');
